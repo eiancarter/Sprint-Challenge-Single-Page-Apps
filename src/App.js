5d018a85d219edec6d73c22eab1d99e04464e0c2
Eian Carter <eian@bricleir.com>
2019-12-20 08:44:19 -0800

    added routes and search

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route, Switch } from 'react-router-dom';
 
 import CharacterList from './components/CharacterList';
+import CharacterCard from './components/CharacterCard';
+import WelcomePage from './components/WelcomePage';
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <Switch>
+        <Route exact path='/'>
+          <WelcomePage />
+        </Route>
+        <Route path='/characters'>
+          <CharacterList/>
+        </Route>
+        <Route path='/characters/:id'>
+          <CharacterCard items={CharacterList} />
+        </Route>
+      </Switch>
     </main>
   );
 }
