1bbb4b36b2968a0e45b696119db14696bd275a98
Eian Carter <eian@bricleir.com>
2019-12-20 08:32:05 -0800

    used axios call to render data on character page

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { Card, CardBody, CardHeader, CardSubtitle} from 'reactstrap';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div className='character-list' key={props.id}>
+      <Card>
+        <CardHeader>{props.name}</CardHeader>
+        <CardBody>
+          <CardSubtitle>{props.status}</CardSubtitle>
+        </CardBody>
+      </Card>
+    </div>
+    );
 }
