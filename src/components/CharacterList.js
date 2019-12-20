1e1b5b280c53cfde73202809d96853a272b2ddde
Eian Carter <eian@bricleir.com>
2019-12-20 09:54:39 -0800

    fixed search term

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 import CharacterCard from './CharacterCard';
+import SearchForm from './SearchForm';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -23,6 +24,9 @@ export default function CharacterList() {
 
   return (
     <section className="character-list">
+      <div>
+        <SearchForm />
+      </div>
       {characters.map(character => {
         return (
           <CharacterCard key={character.id} name={character.name} status={character.status}/>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import CharacterCard from "./CharacterCard";
+
 
 export default function SearchForm() {
- 
+  const [characters, setCharacters] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.log('you have never seen Rick and Morty');
+      })
+    const results = characters.filter(character => {
+      return character.name.toLowerCase().includes(searchTerm.toLowerCase())
+    });
+    setSearchResults(results);
+  }, [searchTerm]);
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form>
+        <label htmlFor='name'>Search:</label>
+        <input 
+          id='name' 
+          type='text' 
+          name='textfield' 
+          placeholder='search for character' 
+          value={searchTerm} 
+          onChange={handleChange} />
+      </form>
+      <div className='character-list'>
+        <ul>
+          {searchResults.map(character => (
+            <CharacterCard name={character.name} status={character.status} />
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
